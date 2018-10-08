bc3e40f48ce2a5aa6d869042f2d77eafec8f1f67
ksw <zenuine1@gmail.com>
2018-10-08 15:27:51 +0900

    ForeignKey모델 코드 추가

--- app/config/settings.py
+++ app/config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'fields',
+    'foreignkey',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
