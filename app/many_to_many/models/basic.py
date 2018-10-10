57216e77039ec970621a3da897062eef17b81e06
ksw <zenuine1@gmail.com>
2018-10-10 10:57:32 +0900

    many_to_many.models 패키지화

--- app/many_to_many/models/basic.py
+++ app/many_to_many/models/basic.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+__all__ = (
+    'Topping',
+    'Pizza',
+)
+
 
 class Topping(models.Model):
     name = models.CharField(max_length=50)
@@ -14,14 +19,3 @@ class Pizza(models.Model):
 
     def __str__(self):
         return self.name
-
-
-# class Friends(models.Model):
-#     name = models.CharField(max_length=50)
-
-class FacebookUser(models.Model):
-    name = models.CharField(max_length=50)
-    friends = models.ForeignKey(
-        'self',
-    )
-
